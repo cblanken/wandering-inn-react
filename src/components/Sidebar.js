e5918a74feed8028d95df129340568939d3ff900
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-02-02 17:13:27 -0500

    Add routes with react-router-dom
    Add navigation via Link(s) in Sidebar.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,6 +9,11 @@ import { FiInfo } from 'react-icons/fi'
 import { FiSettings } from 'react-icons/fi'
 import { FiGithub } from 'react-icons/fi'
 
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from '../pages/Home'
+import Overview from '../pages/Overview'
+import Characters from '../pages/Characters'
+
 const Main = styled.div`
   display: flex;
   flex-direction: column;
@@ -18,15 +23,13 @@ const Main = styled.div`
   width: fit-content;
   padding: 1rem;
 `
-const Section = styled.section`
-`
 
-const SubSection = styled.section`
+const Section = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
-  gap: 1rem;
+  gap: 0.5rem;
   border-bottom: 2px solid rgb(230, 230, 230);
   padding: 1rem 0;
 
@@ -43,24 +46,31 @@ const Footer = styled.footer`
 
 function Sidebar() {
   return (
-    <>
+    <Router>
       <Main>
-        <Section>
-          <SubSection>
-            <SidebarItem text="Overview" icon=<FiActivity/> />
-            <SidebarItem text="Characters" icon=<FiUsers/> />
-            <SidebarItem text="Search" icon=<FiSearch/> />
-          </SubSection>
-          <SubSection>
-            <SidebarItem text="Source" icon=<FiGithub/> />
-          </SubSection>
-        </Section>
+        <div>
+          <Section>
+            <SidebarItem text="Overview" link="/" icon=<FiActivity/> />
+            <SidebarItem text="Characters" link="characters" icon=<FiUsers/> />
+            <SidebarItem text="Search" link="search" icon=<FiSearch/> />
+          </Section>
+          <Section>
+            <SidebarItem text="Source" link="https://github.com/cblanken/wandering-inn-react" icon=<FiGithub/> />
+          </Section>
+        </div>
         <Footer>
           <SidebarItem text="About" icon=<FiInfo/> />
           <SidebarItem text="Settings" icon=<FiSettings/> />
         </Footer>
       </Main>
-    </>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/search" element={<Characters />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/SidebarItem.js
+++ src/components/SidebarItem.js
@@ -1,32 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
 
-function SidebarItem({ text, icon, selected }) {
+const ItemWrapper = styled.div`
+  width: 100%;
 
-  const Button = styled.button`
-    display: flex;
-    flex-direction: row;
-    gap: 0.5rem;
-    justify-content: flex-start;
-    align-items: center;
-    border: none;
-    border-radius: 0.5rem;
-    color: rgb(150,160,160);
-    background: rgba(0,0,0,0);
+  & > a {
     width: 100%;
-    padding: 0.5rem 1.5rem;
-    font: bold 1rem 'Hack', Helvetica, monospace;
+    text-decoration: none;
+  }
 
-    &:hover {
-      color: rgb(210,220,220);
-      background: rgba(0,0,0,30%);
-    }
-  `;
+  &:hover {
+    color: rgb(210,220,220);
+    background: rgba(0,0,0,30%);
+    border-radius: 0.5rem;
+  }
+`
 
+const Item = styled.button`
+  display: flex;
+  flex-direction: row;
+  gap: 0.5rem;
+  justify-content: flex-start;
+  align-items: center;
+  border: none;
+  color: rgb(150,160,160);
+  background: rgba(0,0,0,0);
+  padding: 0.75rem 1.5rem;
+  font: bold 1rem 'Hack', Helvetica, monospace;
+`;
+
+const activeNavLink = {
+  background: "red",
+}
+
+function SidebarItem({ text, icon, link }) {
   return (
-    <>
-        <Button>{icon}{text}</Button>
-    </>
+    <ItemWrapper>
+      {
+        link
+        ? <NavLink 
+            to={link}
+            children=<Item>{icon}{text}</Item>
+            style={({ isActive }) => isActive ? activeNavLink : undefined }
+          />
+        : <Item>{icon}{text}</Item>
+      }
+    </ItemWrapper>
   )
 }
 
